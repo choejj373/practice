25f73517a782d6d147ceddab566feaf30954f5ab
choe jong jin <choejj373@gmail.com>
2023-06-14 15:56:34 +0900

    bodyparser setting 수정

--- app/app.js
+++ app/app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( "extends:true" ));
+app.use(bodyParser.urlencoded( { extended:true } ));
 
 app.use('/', home);
 
